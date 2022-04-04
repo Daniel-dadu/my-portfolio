f9b9c534002cabce1533c4c06bd47a055eea888e
Daniel-dadu <67434516+Daniel-dadu@users.noreply.github.com>
2022-04-03 23:55:35 -0500

    Finished nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,15 +3,26 @@ import './nav.css'
 import {AiFillHome, AiTwotoneMessage} from 'react-icons/ai'
 import {FaUserCircle, FaHandsHelping} from 'react-icons/fa'
 import {MdWork} from 'react-icons/md'
+import {useState} from 'react'
 
 const Nav = () => {
+
+  const [activeNav, setActiveNav] = useState('#')
+
+  let compareActive = id => activeNav === id ? 'active' : ''
+  // let onClickActive = id => setActiveNav(id)
+
   return (
     <nav>
-      <a href="#"><AiFillHome/></a>
-      <a href="#about"><FaUserCircle/></a>
-      <a href="#experience"><MdWork/></a>
-      <a href="#services"><FaHandsHelping/></a>
-      <a href="#contact"><AiTwotoneMessage/></a>
+      <a href="#" onClick={() => setActiveNav("#")} className={compareActive("#")}><AiFillHome/></a>
+
+      <a href="#about" onClick={() => setActiveNav("#about")} className={compareActive("#about")}><FaUserCircle/></a>
+
+      <a href="#experience" onClick={() => setActiveNav("#experience")} className={compareActive("#experience")}><MdWork/></a>
+
+      <a href="#services" onClick={() => setActiveNav("#services")} className={compareActive("#services")}><FaHandsHelping/></a>
+      
+      <a href="#contact" onClick={() => setActiveNav("#contact")} className={compareActive("#contact")}><AiTwotoneMessage/></a>
     </nav>
   )
 }
